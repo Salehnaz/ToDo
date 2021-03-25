0b08d752cd30fb62bfa050974bf43de2ef9bcf89
Salehnaz <salehnazimudeen6@gmail.com>
2021-03-24 23:23:30 -0700

    added a FAB txtview res

--- app/src/main/java/com/example/todo/MainActivity.java
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.todo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton fAction;
+    TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fAction =findViewById(R.id.fab);
+        txt =  findViewById(R.id.textid);
+
+        fAction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                txt.setText("clicked");
+
+            }
+        });
+
+
     }
 }
