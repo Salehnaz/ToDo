168c9e91af8d458b2a6bbc02afe877c7da98a1db
Salehnaz <salehnazimudeen6@gmail.com>
2021-03-28 21:46:29 -0700

    added a submit button

--- app/src/main/java/com/example/todo/EditingActivity.java
+++ app/src/main/java/com/example/todo/EditingActivity.java
@@ -3,12 +3,30 @@ package com.example.todo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class EditingActivity extends AppCompatActivity {
+
+    EditText editText ;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editing);
+
+        editText = (EditText) findViewById(R.id.Etext);
+        button = (Button) findViewById(R.id.add_btn);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String task = editText.getText().toString();
+
+            }
+        });
+
     }
 }
